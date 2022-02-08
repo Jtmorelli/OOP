d79ebee42b8103f6ef6ae739f14bc7ac4044418c
Jake Morelli <jake.morelli98@gmail.com>
2022-02-07 20:54:49 -0600

    2/7 class

---
+++ Fly.py
@@ -0,0 +1,10 @@
+import InsectClass as i
+
+
+def main():
+    new_bug = i.Insect()
+
+    print("This bug can fly ", new_bug.get_Length(), "miles.")
+
+
+main()
---
+++ InsectClass.py
@@ -0,0 +1,11 @@
+import random
+
+
+class Insect:
+    def __init__(self):
+        self.wings = 2
+        self.legs = 4
+        self.LoF = 0
+
+    def get_Length(self):
+        return self.LoF + random.randint(1, 10)
